f8c365f42b31b112e969195e845c75e33a57a23b
Maurer Krisztian <maurerkrisztian@gmail.com>
2021-05-19 20:51:01 +0200

    folded menu by default

--- displayMenu.js
+++ displayMenu.js
@@ -2,7 +2,7 @@
 
 function setVisibility(id) {
   var x = document.getElementById(id);
-  if (x.style.display === "none") {
+  if (x.style.display === "none" || !x.style.display) {
     x.style.display = "block";
   } else {
     x.style.display = "none";
